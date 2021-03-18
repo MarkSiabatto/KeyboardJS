4d97b25f73a7b53c0c80fc64e72cac8325d0c984
MarcoSiabatto <siabattorodriguezmarco@gmail.com>
2021-03-17 19:54:28 -0500

    Have created basic architecture using buttons and icons. Starting on functionality.

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+// Display
+let display=document.getElementById("display");
+// Buttons
+// Numbers Row
+let btn1=document.getElementById("btn1");
+let btn2=document.getElementById("btn2");
+let btn3=document.getElementById("btn3");
+let btn4=document.getElementById("btn4");
+let btn5=document.getElementById("btn5");
+let btn6=document.getElementById("btn6");
+let btn7=document.getElementById("btn7");
+let btn8=document.getElementById("btn8");
+let btn9=document.getElementById("btn9");
+let btn0=document.getElementById("btn0");
+// First row of letters
+let btnQ=document.getElementById("btnQ");
+let btnW=document.getElementById("btnW");
+let btnE=document.getElementById("btnE");
+let btnR=document.getElementById("btnR");
+let btnT=document.getElementById("btnT");
+let btnY=document.getElementById("btnY");
+let btnU=document.getElementById("btnU");
+let btnI=document.getElementById("btnI");
+let btnO=document.getElementById("btnO");
+let btnP=document.getElementById("btnP");
+// Second row of letters
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+// Third row of letter
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+let btn1=document.getElementById("btn1");
+// Git and Spacebar Buttons
+let btnGh=document.getElementById("btnGh");
+let btnSpace=document.getElementById("btnSpaces");
